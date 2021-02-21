81746627ee946b75477eee58b6cf6efd4ae675d0
James Ng <james.ng@cloudsolutions.co.uk>
2021-02-21 17:16:07 +0000

    adjusting GPIO pin

--- main.py
+++ main.py
@@ -14,7 +14,27 @@ def main(plant: Plant):
             f_data = f"Current Time: {current_time} - Temp: {result['temp_c']} - Humidity: {result['humidity']}\n"
             if result['valid'] == True:
                 print(f_data)
-                with open("data.txt", "a") as f:
+                with import RPi.GPIO as GPIO
+
+class MoistureSensor():
+
+    def __init__(self, gpio):
+        self.gpio = gpio
+
+        # Initialse the GPIO pins and LEDs
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+
+        GPIO.setup(self.gpio, GPIO.IN)
+
+    def get_moisture_state(self):
+
+        """ Returns True if the plant needs water and False if not """
+
+        if GPIO.input(self.gpio):
+            return True
+        else:
+            return Falseopen("data.txt", "a") as f:
                     f.write(f_data)
                 plant.check_temperature(result['temp_c'])
                 plant.check_moisture()
@@ -29,7 +49,7 @@ if __name__ == '__main__':
     schefflera = Plant("schefflera", 
                         humidity=(50, 90), 
                         temperature=(12, 22), 
-                        moisture_sensor=MoistureSensor(22), 
+                        moisture_sensor=MoistureSensor(26), 
                         temperature_sensor=DHT22(4), 
                         green=LED(18, "green"), 
                         blue=LED(24, "green"), 
