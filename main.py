da24d626af845a87ccc01267dd87d38157822a6c
James Ng <james.ng@cloudsolutions.co.uk>
2021-03-02 12:53:34 +0000

    fixed the main method

--- main.py
+++ main.py
@@ -14,33 +14,13 @@ def main(plant: Plant):
             f_data = f"Current Time: {current_time} - Temp: {result['temp_c']} - Humidity: {result['humidity']}\n"
             if result['valid'] == True:
                 print(f_data)
-                with import RPi.GPIO as GPIO
-
-class MoistureSensor():
-
-    def __init__(self, gpio):
-        self.gpio = gpio
-
-        # Initialse the GPIO pins and LEDs
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
-
-        GPIO.setup(self.gpio, GPIO.IN)
-
-    def get_moisture_state(self):
-
-        """ Returns True if the plant needs water and False if not """
-
-        if GPIO.input(self.gpio):
-            return True
-        else:
-            return Falseopen("data.txt", "a") as f:
+                with open("data.txt", "a") as f:
                     f.write(f_data)
                 plant.check_temperature(result['temp_c'])
                 plant.check_moisture()
                 time.sleep(10)
         
-        except KeyboardInterrupt:
+        except:
             LED.turn_off_all_leds()
             sys.exit(0)
 
