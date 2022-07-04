ad3c03ab1102e4254a0c8ea7cb56829c6decc3d2
James Ng <james_ng1@hotmail.co.uk>
2022-07-04 18:02:51 +0100

    Add moisture logic

--- plants.py
+++ plants.py
@@ -6,21 +6,40 @@ from pigpio_dht import DHT22
 from moisture_sensor import MoistureSensor
 
 
-class Plant():
-
-    def __init__(self, name: str, humidity: tuple, temperature: tuple, moisture_sensor: MoistureSensor, temperature_sensor: DHT22, green: LED, blue: LED, red: LED):
+class Plant:
+    def __init__(
+        self,
+        name: str,
+        humidity: tuple,
+        temperature: tuple,
+        moisture: tuple,
+        moisture_sensor: MoistureSensor,
+        temperature_sensor: DHT22,
+        green: LED,
+        blue: LED,
+        red: LED,
+        water_pump: WaterPump,
+        secs_to_water: int
+    ):
         self.name = name
         self.humidity = humidity
         self.temperature = temperature
+        self.moisture = moisture
         self.moisture_sensor = moisture_sensor
         self.temperature_sensor = temperature_sensor
         self.green = green
         self.red = red
         self.blue = blue
+        self.water_pump = water_pump
+        self.secs_to_water = secs_to_water
 
-        assert temperature[0] < temperature[1], "The lower temperature threshold comes first"
+        assert (
+            temperature[0] < temperature[1]
+        ), "The lower temperature threshold comes first"
 
         assert humidity[0] < humidity[1], "The lower humidity threshold comes first"
+
+        assert moisture[0] < moisture[1], "The lower moisture threshold comes first"
 
     def check_temperature(self, current_temp: float):
         """ Checks the temperature of the plant. Performs actions to bring the temperature within a specified range """
@@ -28,20 +47,20 @@ class Plant():
         if self.temperature[0] <= current_temp <= self.temperature[1]:
 
             # This is the desired state
-            print("this is desired")
+            print("the temperature is within range")
 
             # LED.turn_off_all_leds()
             # self.green.turn_on()
 
         elif current_temp < self.temperature[0]:
 
-            print("this is too cold, the range is: ", self.temperature)
+            print("This is too cold, the range is: ", self.temperature)
             self.heat_up(current_temp)
             LED.turn_off_all_leds()
             self.blue.turn_on()
 
         else:
-            print("this is too hot, the range is: ", self.temperature)
+            print("This is too hot, the range is: ", self.temperature)
             self.cool_down(current_temp)
             LED.turn_off_all_leds()
             self.red.turn_on()
@@ -58,21 +77,29 @@ class Plant():
             # TODO Implement logic for decreasing the humidity
             pass
 
-    def check_moisture(self):
-        """ Checks the moisture of the plant. Performs actions to bring the moisture within a specified range """
+    def check_moisture(self, current_moisture):
+        """ Checks the humidity of the plant. Performs actions to bring the humidity within a specified range """
 
-        if not self.moisture_sensor.get_moisture_state():
+        if self.moisture[0] >= current_moisture >= self.moisture[1]:
             print("No water needed, got plenty")
             LED.turn_off_all_leds()
             self.green.turn_on()
+
+        elif current_moisture > self.moisture[1]:
+            # TODO Implement logic for watering
+            print("Needs watering...")
+            self.water_pump.water(self.secs_to_water)
         else:
-            self.water()
+            print("You've watered too much")
+
+    def get_moisture(self):
+        return self.moisture_sensor.get_moisture_state()
 
     def heat_up(self, current_temperature: float):
         """ Increases the temperature for the plant """
 
         # TODO Implement logic for heating up
-        print("heating up....")
+        print("Heating up....")
         # while current_temperature < self.temperature[0]:
         #     current_temperature += 1
         #     print(current_temperature)
@@ -84,15 +111,9 @@ class Plant():
         """ Decreases the temperature for the plant """
 
         # TODO Implement logic for cooling down
-        print("cooling down....")
+        print("Cooling down....")
         # while current_temperature > self.temperature[1]:
         #     current_temperature -= 1
         #     print(current_temperature)
         #     time.sleep(1)
         # print("cooled!")
-
-    def water(self):
-        """ Releases water for the plant """
-        print("Water")
-        # TODO Implement logic for controlling the water pump
-        pass
